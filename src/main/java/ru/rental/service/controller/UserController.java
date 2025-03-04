b68333404d2cf5c363586bace86898adad61bb65
dmitriy_boyko <krasti-vip@yandex.ru>
2025-03-04 10:45:08 +0200

    STEP-002 update delete html

--- src/main/java/ru/rental/service/controller/UserController.java
+++ src/main/java/ru/rental/service/controller/UserController.java
@@ -1,15 +1,68 @@
 package ru.rental.service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.rental.service.dto.CarDto;
+import ru.rental.service.dto.UserDto;
+import ru.rental.service.service.UserService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
-    public String getAll() {
-        return "user";
+    public String getAllUsers(Model model) {
+        List<UserDto> users = userService.getAll();
+        model.addAttribute("users", users);
+        return "user/index";
+    }
+
+    @GetMapping("/{id}")
+    public String getUserById(@PathVariable("id") int id, Model model) {
+        var user = userService.get(id).orElseThrow(() -> new IllegalArgumentException("user no found id: " + id));
+        model.addAttribute("user", user);
+        return "user/user";
+    }
+
+    @GetMapping("/create")
+    public String getCreateUserForm(Model model) {
+        model.addAttribute("user", new UserDto());
+        return "user/form";
+    }
+
+    @PostMapping
+    public String createUser(@ModelAttribute("user") UserDto userDto) {
+        userService.save(userDto);
+        return "redirect:/user";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUserById(@PathVariable("id") int id) {
+        userService.delete(id);
+        return "redirect:/user";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateUserById(@PathVariable("id") int id, UserDto userDto) {
+        userService.update(id, userDto);
+        return "redirect:/user";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String showUpdateForm(@PathVariable("id") int id, Model model) {
+        var user = userService.get(id).orElseThrow(() -> new IllegalArgumentException("user no found id: " + id));
+        model.addAttribute("user", user);
+        return "user/update";
     }
 }
