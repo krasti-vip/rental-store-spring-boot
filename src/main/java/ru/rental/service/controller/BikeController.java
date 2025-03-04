208c8296af22e414301ee4778eb509ee8275e56e
dmitriy_boyko <krasti-vip@yandex.ru>
2025-03-04 10:45:08 +0200

    STEP-002 update delete html

--- src/main/java/ru/rental/service/controller/BikeController.java
+++ src/main/java/ru/rental/service/controller/BikeController.java
@@ -3,8 +3,7 @@ package ru.rental.service.controller;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.rental.service.dto.BikeDto;
 import ru.rental.service.service.BikeService;
 
@@ -24,8 +23,46 @@ public class BikeController {
     @GetMapping
     public String getAllBikes(Model model) {
         List<BikeDto> bikes = bikeService.getAll();
-        model.addAttribute("bike", bikes);
-        return "bike";
+        model.addAttribute("bikes", bikes);
+        return "bike/index";
+    }
+
+    @GetMapping("/{id}")
+    public String getBikeById(@PathVariable("id") int id, Model model) {
+        var bike = bikeService.get(id).orElseThrow(() -> new IllegalArgumentException("bike no found id: " + id));
+        model.addAttribute("bike", bike);
+        return "bike/bike";
+    }
+
+    @GetMapping("/create")
+    public String getCreateBikeForm(Model model) {
+        model.addAttribute("bike", new BikeDto());
+        return "bike/form";
+    }
+
+    @PostMapping
+    public String createBike(@ModelAttribute("bike") BikeDto bikeDto) {
+        bikeService.save(bikeDto);
+        return "redirect:/bike";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteBikeById(@PathVariable("id") int id) {
+        bikeService.delete(id);
+        return "redirect:/bike";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateBikeById(@PathVariable("id") int id, BikeDto bikeDto) {
+        bikeService.update(id, bikeDto);
+        return "redirect:/bike";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String showUpdateForm(@PathVariable("id") int id, Model model) {
+        var bike = bikeService.get(id).orElseThrow(() -> new IllegalArgumentException("bike no found id: " + id));
+        model.addAttribute("bike", bike);
+        return "bike/update";
     }
 }
 
--- src/main/java/ru/rental/service/controller/CarController.java
+++ src/main/java/ru/rental/service/controller/CarController.java
@@ -1,15 +1,68 @@
 package ru.rental.service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.rental.service.dto.BikeDto;
+import ru.rental.service.dto.CarDto;
+import ru.rental.service.service.CarService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/car")
 public class CarController {
 
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
     @GetMapping
-    public String getAll() {
-        return "car";
+    public String getAllCars(Model model) {
+        List<CarDto> cars = carService.getAll();
+        model.addAttribute("cars", cars);
+        return "car/index";
+    }
+
+    @GetMapping("/{id}")
+    public String getCarById(@PathVariable("id") int id, Model model) {
+        var car = carService.get(id).orElseThrow(() -> new IllegalArgumentException("car no found id: " + id));
+        model.addAttribute("car", car);
+        return "car/car";
+    }
+
+    @GetMapping("/create")
+    public String getCreateCarForm(Model model) {
+        model.addAttribute("car", new CarDto());
+        return "car/form";
+    }
+
+    @PostMapping
+    public String createCar(@ModelAttribute("car") CarDto carDto) {
+        carService.save(carDto);
+        return "redirect:/car";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteCarById(@PathVariable("id") int id) {
+        carService.delete(id);
+        return "redirect:/car";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateCarById(@PathVariable("id") int id, CarDto carDto) {
+        carService.update(id, carDto);
+        return "redirect:/car";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String showUpdateForm(@PathVariable("id") int id, Model model) {
+        var car = carService.get(id).orElseThrow(() -> new IllegalArgumentException("car no found id: " + id));
+        model.addAttribute("car", car);
+        return "car/update";
     }
 }
